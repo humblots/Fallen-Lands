c089ba627fbbe5d94b941b58c87d7e006ef44838
Stephane Humblot <SHumblot@oph74.fr>
2024-01-31 21:01:18 +0100

    added attack cooldown, flip BOD

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -3,27 +3,33 @@ using UnityEngine;
 public class Entity : MonoBehaviour
 {
     public float maxHealth = 200f;
-
-    [SerializeField] public Animator animator;
+    
+    private Animator Animator;
+    private Rigidbody2D RB;
+    private Collider2D Collider;
+    
     private float currentHealth;
 
     private void Start()
     {
+        Animator = GetComponent<Animator>();
+        RB = GetComponent<Rigidbody2D>();
+        Collider = GetComponent<Collider2D>();
         currentHealth = maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
-        animator.SetTrigger("hurt");
+        Animator.SetTrigger("hurt");
         if (currentHealth <= 0) Die();
     }
 
     public void Die()
     {
-        animator.SetBool("isDead", true);
-        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-        GetComponent<Collider2D>().enabled = false;
+        Animator.SetBool("isDead", true);
+        RB.bodyType = RigidbodyType2D.Static;
+        Collider.enabled = false;
         enabled = false;
     }
     
--- Assets/Scripts/Warrior/PlayerAttack.cs
+++ Assets/Scripts/Warrior/PlayerAttack.cs
@@ -1,18 +1,40 @@
+using System;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
     public float attackDamage = 10f;
-    public float attackSpeed = 2f;
     public float attackRange = 2f;
-
+    public float initialAttackDelay = 2f;
+    public float knockbackForce = 1.5f;
+    
+    private float attackDelay = 0f;
+    
+    private PlayerMovement playerMovement;
+    
     [SerializeField] public Animator animator;
     [SerializeField] public Transform attackPoint;
     [SerializeField] public LayerMask enemyLayer;
 
+    private void Start()
+    { 
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) Attack();
+        if (attackDelay <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+                attackDelay = initialAttackDelay;
+            }
+        }
+        else
+        {
+            attackDelay -= Time.deltaTime;
+        }
     }
 
     private void OnDrawGizmosSelected()
@@ -25,6 +47,12 @@ public class PlayerAttack : MonoBehaviour
     {
         animator.SetTrigger("attack");
         var hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
-        foreach (var enemy in hitEnemies) enemy.GetComponent<Entity>().TakeDamage(attackDamage);
+        foreach (var enemy in hitEnemies)
+        {
+            var knockBackDir = playerMovement.IsFacingRight ? 1f : -1f;
+            enemy.GetComponent<Rigidbody2D>()
+                .AddForce(new Vector2(knockBackDir * knockbackForce, 0f), ForceMode2D.Impulse);
+            enemy.GetComponent<Entity>().TakeDamage(attackDamage);
+        }
     }
 }
